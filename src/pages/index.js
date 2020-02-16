d7f16721fc884ea1bbd5fa0d986a8b3612e80dc1
R Sai Charan Tej <r.saicharantej@gmail.com>
2020-02-16 21:45:48 +0530

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,53 @@
-import React from "react"
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-export default () => <div>Hello world!</div>
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+});
+
+export default function SimpleCard() {
+    const classes = useStyles();
+    const bull = <span className={classes.bullet}>•</span>;
+
+    return (
+        <Card className={classes.root}>
+            <CardContent>
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                    Ramamurthy Vishwakarma
+        </Typography>
+                <Typography variant="h5" component="h2">
+                    Organisational Secretry
+        </Typography>
+                <Typography className={classes.pos} color="textSecondary">
+                    Akhila Bharatiya Swarnakar Sangh
+        </Typography>
+                <Typography variant="body2" component="p">
+                    New Delhi
+          <br />
+                    {'"Jai Vishwakarma"'}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Learn More About Ramamurthy</Button>
+            </CardActions>
+        </Card>
+    );
+}
